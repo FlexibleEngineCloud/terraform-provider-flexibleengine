57138a5dda0a5a1b730a3129d000232eae03f767
freesky-edward <freesky.edward@gmail.com>
2018-01-24 08:03:10 +0000

    Refactory to support new project solution of framework

--- flexibleengine/resource_flexibleengine_as_configuration_v1.go
+++ flexibleengine/resource_flexibleengine_as_configuration_v1.go
@@ -352,7 +352,7 @@ func resourceASConfigurationDelete(d *schema.ResourceData, meta interface{}) err
 	return nil
 }
 
-func getASGroupsByConfiguration(asClient *gophercloud.ServiceClient, configurationID string) ([]groups.Group, error) {
+func getASGroupsByConfiguration(asClient *gophercloud.ServiceClientExtension, configurationID string) ([]groups.Group, error) {
 	var gs []groups.Group
 	listOpts := groups.ListOpts{
 		ConfigurationID: configurationID,
