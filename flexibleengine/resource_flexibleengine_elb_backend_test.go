714e614898283ebaddc5cad4a1fef174550083c4
huangtianhua <huangtianhua@huawei.com>
2018-01-27 16:00:26 +0800

    Fix acctest: TestAccELBBackend_basic

--- flexibleengine/resource_flexibleengine_elb_backend_test.go
+++ flexibleengine/resource_flexibleengine_elb_backend_test.go
@@ -7,10 +7,13 @@ import (
 	//"github.com/huawei-clouds/golangsdk/openstack/networking/v2/extensions/elbaas/backendmember"
 	"github.com/hashicorp/terraform/helper/resource"
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/huawei-clouds/golangsdk/openstack/networking/v2/extensions/elbaas/backendmember"
+	"log"
 )
 
 // PASS with diff
 func TestAccELBBackend_basic(t *testing.T) {
+	var backend backendmember.Backend
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
@@ -21,14 +24,7 @@ func TestAccELBBackend_basic(t *testing.T) {
 				Config:             TestAccELBBackendConfig_basic,
 				ExpectNonEmptyPlan: true, // Because admin_state_up remains false, unfinished elb?
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckELBBackendExists("flexibleengine_elb_backend.backend_1"),
-				),
-			},
-			resource.TestStep{
-				Config:             TestAccELBBackendConfig_update,
-				ExpectNonEmptyPlan: true, // Because admin_state_up remains false, unfinished elb?
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("flexibleengine_elb_backend.backend_1", "weight", "10"),
+					testAccCheckELBBackendExists("flexibleengine_elb_backend.backend_orange_acctest", &backend),
 				),
 			},
 		},
@@ -37,7 +33,7 @@ func TestAccELBBackend_basic(t *testing.T) {
 
 func testAccCheckELBBackendDestroy(s *terraform.State) error {
 	config := testAccProvider.Meta().(*Config)
-	_, err := config.otcV1Client(OS_REGION_NAME)
+	client, err := config.otcV1Client(OS_REGION_NAME)
 	if err != nil {
 		return fmt.Errorf("Error creating OrangeCloud networking client: %s", err)
 	}
@@ -47,12 +43,16 @@ func testAccCheckELBBackendDestroy(s *terraform.State) error {
 			continue
 		}
 
+		_, err := backendmember.Get(client, rs.Primary.Attributes["listener_id"], rs.Primary.ID).Extract()
+		if err == nil {
+			return fmt.Errorf("Backend member still exists: %s", rs.Primary.ID)
+		}
 	}
 
 	return nil
 }
 
-func testAccCheckELBBackendExists(n string) resource.TestCheckFunc {
+func testAccCheckELBBackendExists(n string, backend *backendmember.Backend) resource.TestCheckFunc {
 	return func(s *terraform.State) error {
 		rs, ok := s.RootModule().Resources[n]
 		if !ok {
@@ -64,106 +64,78 @@ func testAccCheckELBBackendExists(n string) resource.TestCheckFunc {
 		}
 
 		config := testAccProvider.Meta().(*Config)
-		_ /*networkingClient*/, err := config.otcV1Client(OS_REGION_NAME)
+		client, err := config.otcV1Client(OS_REGION_NAME)
 		if err != nil {
 			return fmt.Errorf("Error creating OrangeCloud networking client: %s", err)
 		}
+
+		founds, err := backendmember.Get(client, rs.Primary.Attributes["listener_id"], rs.Primary.ID).Extract()
+		if err != nil {
+			return err
+		}
+		log.Printf("[DEBUG] testAccCheckELBBackendExists found %+v.\n", founds)
+		found := founds[0]
+		if found.ID != rs.Primary.ID {
+			return fmt.Errorf("Backend member not found")
+		}
+
+		*backend = found
 
 		return nil
 	}
 }
 
 var TestAccELBBackendConfig_basic = fmt.Sprintf(`
-resource "flexibleengine_elb_loadbalancer" "loadbalancer_1" {
-  name = "loadbalancer_1"
-  // vip_subnet_id = "${flexibleengine_networking_subnet_v2.subnet_1.id}"
+resource "flexibleengine_elb_loadbalancer" "lb_orange_acctest" {
+  name = "lb_orange_acctest"
   vpc_id = "%s"
   type = "External"
+  bandwidth = 5
 }
 
-resource "flexibleengine_elb_listener" "listener_1" {
-  name = "listener_1"
-  protocol = "HTTP"
+resource "flexibleengine_elb_listener" "ls_orange_acctest" {
+  name = "ls_orange_acctest"
+  protocol = "TCP"
   protocol_port = 8080
-  loadbalancer_id = "${flexibleengine_elb_loadbalancer.loadbalancer_1.id}"
+  backend_protocol = "TCP"
+  backend_port = 8080
+  lb_algorithm = "roundrobin"
+  loadbalancer_id = "${flexibleengine_elb_loadbalancer.lb_orange_acctest.id}"
 }
 
-resource "flexibleengine_elb_backend" "backend_1" {
-  address = "192.168.199.10"
-  subnet_id = "${flexibleengine_networking_subnet_v2.subnet_1.id}"
-  listener_id = "${flexibleengine_elb_listener.listener_1.id}"
-  server_id = "gary-backend"
+
+resource "flexibleengine_elb_health" "health_orange_acctest" {
+  listener_id = "${flexibleengine_elb_listener.ls_orange_acctest.id}"
+  healthcheck_protocol = "HTTP"
+  healthy_threshold = 3
+  healthcheck_timeout = 10
+  healthcheck_interval = 5
+
   timeouts {
     create = "5m"
-    update = "5m"
     delete = "5m"
   }
 }
 
-resource "flexibleengine_elb_backend" "backend_2" {
-  address = "192.168.199.11"
-  protocol_port = 8080
-  subnet_id = "${flexibleengine_networking_subnet_v2.subnet_1.id}"
-
-  timeouts {
-    create = "5m"
-    update = "5m"
-    delete = "5m"
-  }
-}
-`, OS_VPC_ID)
-
-const TestAccELBBackendConfig_update = `
-resource "flexibleengine_networking_network_v2" "network_1" {
-  name = "network_1"
-  admin_state_up = "true"
+resource "flexibleengine_compute_secgroup_v2" "secgroup_orange_acc_test" {
+  name = "secgroup_orange_acc_test"
 }
 
-resource "flexibleengine_networking_subnet_v2" "subnet_1" {
-  name = "subnet_1"
-  cidr = "192.168.199.0/24"
-  ip_version = 4
-  network_id = "${flexibleengine_networking_network_v2.network_1.id}"
-}
-
-resource "flexibleengine_elb_loadbalancer" "loadbalancer_1" {
-  name = "loadbalancer_1"
-  vip_subnet_id = "${flexibleengine_networking_subnet_v2.subnet_1.id}"
-}
-
-resource "flexibleengine_elb_listener" "listener_1" {
-  name = "listener_1"
-  protocol = "HTTP"
-  protocol_port = 8080
-  loadbalancer_id = "${flexibleengine_elb_loadbalancer.loadbalancer_1.id}"
-}
-
-
-resource "flexibleengine_elb_backend" "backend_1" {
-  address = "192.168.199.10"
-  protocol_port = 8080
-  weight = 10
-  admin_state_up = "true"
-  subnet_id = "${flexibleengine_networking_subnet_v2.subnet_1.id}"
-
-  timeouts {
-    create = "5m"
-    update = "5m"
-    delete = "5m"
+resource "flexibleengine_compute_instance_v2" "instance_orange_backend_test" {
+  name = "instance_orange_backend_test"
+  security_groups = ["${flexibleengine_compute_secgroup_v2.secgroup_orange_acc_test.name}"]
+  network {
+    uuid = "%s"
   }
 }
 
-resource "flexibleengine_elb_backend" "backend" {
-  address = "192.168.199.11"
-  protocol_port = 8080
-  weight = 15
-  admin_state_up = "true"
-  subnet_id = "${flexibleengine_networking_subnet_v2.subnet_1.id}"
-
+resource "flexibleengine_elb_backend" "backend_orange_acctest" {
+  address = "${flexibleengine_compute_instance_v2.instance_orange_backend_test.access_ip_v4}"
+  listener_id = "${flexibleengine_elb_listener.ls_orange_acctest.id}"
+  server_id = "${flexibleengine_compute_instance_v2.instance_orange_backend_test.id}"
   timeouts {
     create = "5m"
-    update = "5m"
     delete = "5m"
   }
 }
-`
+`, OS_VPC_ID, OS_NETWORK_ID)
