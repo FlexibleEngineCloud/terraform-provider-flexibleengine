1ae951348000d09e0562b2c83a3a38918021fe07
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2018-08-27 16:59:24 +0800

    Add ULB resource support

---
+++ flexibleengine/resource_flexibleengine_lb_listener_v2_test.go
@@ -0,0 +1,130 @@
+package flexibleengine
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/lbaas_v2/listeners"
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccLBV2Listener_basic(t *testing.T) {
+	var listener listeners.Listener
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheckLB(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLBV2ListenerDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: TestAccLBV2ListenerConfig_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLBV2ListenerExists("flexibleengine_lb_listener_v2.listener_1", &listener),
+				),
+			},
+			resource.TestStep{
+				Config: TestAccLBV2ListenerConfig_update,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"flexibleengine_lb_listener_v2.listener_1", "name", "listener_1_updated"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckLBV2ListenerDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*Config)
+	networkingClient, err := config.networkingV2Client(OS_REGION_NAME)
+	if err != nil {
+		return fmt.Errorf("Error creating FlexibleEngine networking client: %s", err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "flexibleengine_lb_listener_v2" {
+			continue
+		}
+
+		_, err := listeners.Get(networkingClient, rs.Primary.ID).Extract()
+		if err == nil {
+			return fmt.Errorf("Listener still exists: %s", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckLBV2ListenerExists(n string, listener *listeners.Listener) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		config := testAccProvider.Meta().(*Config)
+		networkingClient, err := config.networkingV2Client(OS_REGION_NAME)
+		if err != nil {
+			return fmt.Errorf("Error creating FlexibleEngine networking client: %s", err)
+		}
+
+		found, err := listeners.Get(networkingClient, rs.Primary.ID).Extract()
+		if err != nil {
+			return err
+		}
+
+		if found.ID != rs.Primary.ID {
+			return fmt.Errorf("Member not found")
+		}
+
+		*listener = *found
+
+		return nil
+	}
+}
+
+var TestAccLBV2ListenerConfig_basic = fmt.Sprintf(`
+resource "flexibleengine_lb_loadbalancer_v2" "loadbalancer_1" {
+  name = "loadbalancer_1"
+  vip_subnet_id = "%s"
+}
+
+resource "flexibleengine_lb_listener_v2" "listener_1" {
+  name = "listener_1"
+  protocol = "HTTP"
+  protocol_port = 8080
+  loadbalancer_id = "${flexibleengine_lb_loadbalancer_v2.loadbalancer_1.id}"
+
+	timeouts {
+		create = "5m"
+		update = "5m"
+		delete = "5m"
+	}
+}
+`, OS_SUBNET_ID)
+
+var TestAccLBV2ListenerConfig_update = fmt.Sprintf(`
+resource "flexibleengine_lb_loadbalancer_v2" "loadbalancer_1" {
+  name = "loadbalancer_1"
+  vip_subnet_id = "%s"
+}
+
+resource "flexibleengine_lb_listener_v2" "listener_1" {
+  name = "listener_1_updated"
+  protocol = "HTTP"
+  protocol_port = 8080
+  #connection_limit = 100
+  admin_state_up = "true"
+  loadbalancer_id = "${flexibleengine_lb_loadbalancer_v2.loadbalancer_1.id}"
+
+	timeouts {
+		create = "5m"
+		update = "5m"
+		delete = "5m"
+	}
+}
+`, OS_SUBNET_ID)
---
+++ flexibleengine/resource_flexibleengine_lb_loadbalancer_v2_test.go
@@ -0,0 +1,265 @@
+package flexibleengine
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/lbaas_v2/loadbalancers"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/extensions/security/groups"
+	"github.com/gophercloud/gophercloud/openstack/networking/v2/ports"
+	"regexp"
+)
+
+func TestAccLBV2LoadBalancer_basic(t *testing.T) {
+	var lb loadbalancers.LoadBalancer
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheckLB(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLBV2LoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccLBV2LoadBalancerConfig_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLBV2LoadBalancerExists("flexibleengine_lb_loadbalancer_v2.loadbalancer_1", &lb),
+				),
+			},
+			resource.TestStep{
+				Config: testAccLBV2LoadBalancerConfig_update,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"flexibleengine_lb_loadbalancer_v2.loadbalancer_1", "name", "loadbalancer_1_updated"),
+					resource.TestMatchResourceAttr(
+						"flexibleengine_lb_loadbalancer_v2.loadbalancer_1", "vip_port_id",
+						regexp.MustCompile("^[a-f0-9-]+")),
+				),
+			},
+		},
+	})
+}
+
+func TestAccLBV2LoadBalancer_secGroup(t *testing.T) {
+	var lb loadbalancers.LoadBalancer
+	var sg_1, sg_2 groups.SecGroup
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheckLB(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckLBV2LoadBalancerDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccLBV2LoadBalancer_secGroup,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLBV2LoadBalancerExists(
+						"flexibleengine_lb_loadbalancer_v2.loadbalancer_1", &lb),
+					testAccCheckNetworkingV2SecGroupExists(
+						"flexibleengine_networking_secgroup_v2.secgroup_1", &sg_1),
+					testAccCheckNetworkingV2SecGroupExists(
+						"flexibleengine_networking_secgroup_v2.secgroup_1", &sg_2),
+					resource.TestCheckResourceAttr(
+						"flexibleengine_lb_loadbalancer_v2.loadbalancer_1", "security_group_ids.#", "1"),
+					testAccCheckLBV2LoadBalancerHasSecGroup(&lb, &sg_1),
+				),
+			},
+			resource.TestStep{
+				Config: testAccLBV2LoadBalancer_secGroup_update1,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLBV2LoadBalancerExists(
+						"flexibleengine_lb_loadbalancer_v2.loadbalancer_1", &lb),
+					testAccCheckNetworkingV2SecGroupExists(
+						"flexibleengine_networking_secgroup_v2.secgroup_2", &sg_1),
+					testAccCheckNetworkingV2SecGroupExists(
+						"flexibleengine_networking_secgroup_v2.secgroup_2", &sg_2),
+					resource.TestCheckResourceAttr(
+						"flexibleengine_lb_loadbalancer_v2.loadbalancer_1", "security_group_ids.#", "2"),
+					testAccCheckLBV2LoadBalancerHasSecGroup(&lb, &sg_1),
+					testAccCheckLBV2LoadBalancerHasSecGroup(&lb, &sg_2),
+				),
+			},
+			resource.TestStep{
+				Config: testAccLBV2LoadBalancer_secGroup_update2,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckLBV2LoadBalancerExists(
+						"flexibleengine_lb_loadbalancer_v2.loadbalancer_1", &lb),
+					testAccCheckNetworkingV2SecGroupExists(
+						"flexibleengine_networking_secgroup_v2.secgroup_2", &sg_1),
+					testAccCheckNetworkingV2SecGroupExists(
+						"flexibleengine_networking_secgroup_v2.secgroup_2", &sg_2),
+					resource.TestCheckResourceAttr(
+						"flexibleengine_lb_loadbalancer_v2.loadbalancer_1", "security_group_ids.#", "1"),
+					testAccCheckLBV2LoadBalancerHasSecGroup(&lb, &sg_2),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckLBV2LoadBalancerDestroy(s *terraform.State) error {
+	config := testAccProvider.Meta().(*Config)
+	networkingClient, err := config.networkingV2Client(OS_REGION_NAME)
+	if err != nil {
+		return fmt.Errorf("Error creating FlexibleEngine networking client: %s", err)
+	}
+
+	for _, rs := range s.RootModule().Resources {
+		if rs.Type != "flexibleengine_lb_loadbalancer_v2" {
+			continue
+		}
+
+		_, err := loadbalancers.Get(networkingClient, rs.Primary.ID).Extract()
+		if err == nil {
+			return fmt.Errorf("LoadBalancer still exists: %s", rs.Primary.ID)
+		}
+	}
+
+	return nil
+}
+
+func testAccCheckLBV2LoadBalancerExists(
+	n string, lb *loadbalancers.LoadBalancer) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Not found: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("No ID is set")
+		}
+
+		config := testAccProvider.Meta().(*Config)
+		networkingClient, err := config.networkingV2Client(OS_REGION_NAME)
+		if err != nil {
+			return fmt.Errorf("Error creating FlexibleEngine networking client: %s", err)
+		}
+
+		found, err := loadbalancers.Get(networkingClient, rs.Primary.ID).Extract()
+		if err != nil {
+			return err
+		}
+
+		if found.ID != rs.Primary.ID {
+			return fmt.Errorf("Member not found")
+		}
+
+		*lb = *found
+
+		return nil
+	}
+}
+
+func testAccCheckLBV2LoadBalancerHasSecGroup(
+	lb *loadbalancers.LoadBalancer, sg *groups.SecGroup) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		config := testAccProvider.Meta().(*Config)
+		networkingClient, err := config.networkingV2Client(OS_REGION_NAME)
+		if err != nil {
+			return fmt.Errorf("Error creating FlexibleEngine networking client: %s", err)
+		}
+
+		port, err := ports.Get(networkingClient, lb.VipPortID).Extract()
+		if err != nil {
+			return err
+		}
+
+		for _, p := range port.SecurityGroups {
+			if p == sg.ID {
+				return nil
+			}
+		}
+
+		return fmt.Errorf("LoadBalancer does not have the security group")
+	}
+}
+
+var testAccLBV2LoadBalancerConfig_basic = fmt.Sprintf(`
+resource "flexibleengine_lb_loadbalancer_v2" "loadbalancer_1" {
+  name = "loadbalancer_1"
+  vip_subnet_id = "%s"
+
+  timeouts {
+    create = "5m"
+    update = "5m"
+    delete = "5m"
+  }
+}
+`, OS_SUBNET_ID)
+
+var testAccLBV2LoadBalancerConfig_update = fmt.Sprintf(`
+resource "flexibleengine_lb_loadbalancer_v2" "loadbalancer_1" {
+  name = "loadbalancer_1_updated"
+  admin_state_up = "true"
+  vip_subnet_id = "%s"
+
+  timeouts {
+    create = "5m"
+    update = "5m"
+    delete = "5m"
+  }
+}
+`, OS_SUBNET_ID)
+
+var testAccLBV2LoadBalancer_secGroup = fmt.Sprintf(`
+resource "flexibleengine_networking_secgroup_v2" "secgroup_1" {
+  name = "secgroup_1"
+  description = "secgroup_1"
+}
+
+resource "flexibleengine_networking_secgroup_v2" "secgroup_2" {
+  name = "secgroup_2"
+  description = "secgroup_2"
+}
+
+resource "flexibleengine_lb_loadbalancer_v2" "loadbalancer_1" {
+    name = "loadbalancer_1"
+    vip_subnet_id = "%s"
+    security_group_ids = [
+      "${flexibleengine_networking_secgroup_v2.secgroup_1.id}"
+    ]
+}
+`, OS_SUBNET_ID)
+
+var testAccLBV2LoadBalancer_secGroup_update1 = fmt.Sprintf(`
+resource "flexibleengine_networking_secgroup_v2" "secgroup_1" {
+  name = "secgroup_1"
+  description = "secgroup_1"
+}
+
+resource "flexibleengine_networking_secgroup_v2" "secgroup_2" {
+  name = "secgroup_2"
+  description = "secgroup_2"
+}
+
+resource "flexibleengine_lb_loadbalancer_v2" "loadbalancer_1" {
+    name = "loadbalancer_1"
+    vip_subnet_id = "%s"
+    security_group_ids = [
+      "${flexibleengine_networking_secgroup_v2.secgroup_1.id}",
+      "${flexibleengine_networking_secgroup_v2.secgroup_2.id}"
+    ]
+}
+`, OS_SUBNET_ID)
+
+var testAccLBV2LoadBalancer_secGroup_update2 = fmt.Sprintf(`
+resource "flexibleengine_networking_secgroup_v2" "secgroup_1" {
+  name = "secgroup_1"
+  description = "secgroup_1"
+}
+
+resource "flexibleengine_networking_secgroup_v2" "secgroup_2" {
+  name = "secgroup_2"
+  description = "secgroup_2"
+}
+
+resource "flexibleengine_lb_loadbalancer_v2" "loadbalancer_1" {
+    name = "loadbalancer_1"
+    vip_subnet_id = "%s"
+    security_group_ids = [
+      "${flexibleengine_networking_secgroup_v2.secgroup_2.id}"
+    ]
+    depends_on = ["flexibleengine_networking_secgroup_v2.secgroup_1"]
+}
+`, OS_SUBNET_ID)
