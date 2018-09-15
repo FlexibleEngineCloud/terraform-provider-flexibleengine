f9c52681589c6a972694310f04c64b6812818533
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2018-09-15 15:31:06 +0800

    Fix acc test issue

--- flexibleengine/resource_flexibleengine_mrs_job_v1_test.go
+++ flexibleengine/resource_flexibleengine_mrs_job_v1_test.go
@@ -124,9 +124,9 @@ resource "flexibleengine_mrs_job_v1" "job1" {
   job_type = 1
   job_name = "test_mapreduce_job1"
   cluster_id = "${flexibleengine_mrs_cluster_v1.cluster1.id}"
-  jar_path = "s3a://wordcount/program/hadoop-mapreduce-examples-2.7.5.jar"
-  input = "s3a://wordcount/input/"
-  output = "s3a://wordcount/output/"
-  job_log = "s3a://wordcount/log/"
+  jar_path = "s3a://tf-mrs/program/hadoop-mapreduce-examples-2.7.5.jar"
+  input = "s3a://tf-mrs/input/"
+  output = "s3a://tf-mrs/output/"
+  job_log = "s3a://tf-mrs/joblog/"
   arguments = "wordcount"
 }`, OS_REGION_NAME, OS_AVAILABILITY_ZONE, OS_VPC_ID, OS_VPC_NAME, OS_NETWORK_ID, OS_SUBNET_NAME)
