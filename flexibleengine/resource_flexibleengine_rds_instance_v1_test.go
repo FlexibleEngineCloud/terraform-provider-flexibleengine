1edfdd6dd81e7a6434259ab923e7c39d317b4c18
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2018-12-18 11:43:59 +0800

    Add PostgreSQL support to RDS

--- flexibleengine/resource_flexibleengine_rds_instance_v1_test.go
+++ flexibleengine/resource_flexibleengine_rds_instance_v1_test.go
@@ -30,6 +30,26 @@ func TestAccRDSV1Instance_basic(t *testing.T) {
 	})
 }
 
+func TestAccRDSV1Instance_PostgreSQL(t *testing.T) {
+	var instance instances.Instance
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckRDSV1InstanceDestroy,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: TestAccSInstanceV1Config_PostgreSQL,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckRDSV1InstanceExists("flexibleengine_rds_instance_v1.instance", &instance),
+					resource.TestCheckResourceAttr(
+						"flexibleengine_rds_instance_v1.instance", "status", "ACTIVE"),
+				),
+			},
+		},
+	})
+}
+
 func testAccCheckRDSV1InstanceDestroy(s *terraform.State) error {
 	config := testAccProvider.Meta().(*Config)
 	rdsClient, err := config.RdsV1Client(OS_REGION_NAME)
@@ -102,6 +122,52 @@ resource "flexibleengine_rds_instance_v1" "instance" {
   datastore {
     type = "MySQL"
     version = "5.6.30"
+  }
+  flavorref = "${data.flexibleengine_rds_flavors_v1.flavor.id}"
+  volume {
+    type = "COMMON"
+    size = 100
+  }
+  region = "eu-west-0"
+  availabilityzone = "eu-west-0a"
+  vpc = "%s"
+  nics {
+    subnetid = "%s"
+  }
+  securitygroup {
+    id = "${flexibleengine_networking_secgroup_v2.secgrp_rds.id}"
+  }
+  dbport = "8635"
+  backupstrategy = {
+    starttime = "00:00:00"
+    keepdays = 0
+  }
+  dbrtpd = "Huangwei-120521"
+  ha = {
+    enable = true
+    replicationmode = "async"
+  }
+  depends_on = ["flexibleengine_networking_secgroup_v2.secgrp_rds"]
+}`, OS_VPC_ID, OS_NETWORK_ID)
+
+var TestAccSInstanceV1Config_PostgreSQL = fmt.Sprintf(`
+data "flexibleengine_rds_flavors_v1" "flavor" {
+    region = "eu-west-0"
+    datastore_name = "PostgreSQL"
+    datastore_version = "9.5.5"
+    speccode = "rds.pg.s1.medium.ha"
+}
+
+resource "flexibleengine_networking_secgroup_v2" "secgrp_rds" {
+  name        = "secgrp-rds-instance"
+  description = "Rds Security Group"
+}
+
+resource "flexibleengine_rds_instance_v1" "instance" {
+  name = "rds-instance"
+  datastore {
+    type = "PostgreSQL"
+    version = "9.5.5"
   }
   flavorref = "${data.flexibleengine_rds_flavors_v1.flavor.id}"
   volume {
