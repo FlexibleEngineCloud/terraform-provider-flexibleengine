d73401f7dce4a77983ca3836946e402bd8547efb
Zhenguo Niu <Niu.ZGlinux@gmail.com>
2019-09-27 17:55:28 +0800

    Add DDS flavor data source

---
+++ flexibleengine/data_source_flexibleengine_dds_flavors_v3_test.go
@@ -0,0 +1,48 @@
+package flexibleengine
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform/helper/resource"
+	"github.com/hashicorp/terraform/terraform"
+)
+
+func TestAccDDSFlavorV3DataSource_basic(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDDSFlavorV3DataSource_basic,
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDNSZoneV2DataSourceID("data.flexibleengine_dds_flavor_v3.flavor"),
+					resource.TestCheckResourceAttr(
+						"data.flexibleengine_dds_flavor_v3.flavor", "engine_name", "DDS-Community"),
+				),
+			},
+		},
+	})
+}
+
+func testAccCheckDDSFlavorV3DataSourceID(n string) resource.TestCheckFunc {
+	return func(s *terraform.State) error {
+		rs, ok := s.RootModule().Resources[n]
+		if !ok {
+			return fmt.Errorf("Can't find DDS Flavor data source: %s", n)
+		}
+
+		if rs.Primary.ID == "" {
+			return fmt.Errorf("DDS Flavor data source ID not set")
+		}
+
+		return nil
+	}
+}
+
+var testAccDDSFlavorV3DataSource_basic = `
+data "flexibleengine_dds_flavor_v3" "flavor" {
+    region = "eu-west-0"
+	engine_name = "DDS-Community"
+}
+`
