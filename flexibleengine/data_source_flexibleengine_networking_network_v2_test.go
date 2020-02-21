a4ff9b1b6f1a99663bafdbf8d0d1ca59b800aa46
ShiChangkuo <shichangkuo90@gmail.com>
2020-02-21 18:19:19 +0800

    Update test case for data.flexibleengine_networking_network_v2

--- flexibleengine/data_source_flexibleengine_networking_network_v2_test.go
+++ flexibleengine/data_source_flexibleengine_networking_network_v2_test.go
@@ -2,72 +2,41 @@ package flexibleengine
 
 import (
 	"fmt"
+	"math/rand"
 	"testing"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
-func TestAccFlexibleEngineNetworkingNetworkV2DataSource_basic(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccFlexibleEngineNetworkingNetworkV2DataSource_network,
-			},
-			{
-				Config: testAccFlexibleEngineNetworkingNetworkV2DataSource_basic,
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckNetworkingNetworkV2DataSourceID("data.flexibleengine_networking_network_v2.net"),
-					resource.TestCheckResourceAttr(
-						"data.flexibleengine_networking_network_v2.net", "name", "flexibleengine_test_network"),
-					resource.TestCheckResourceAttr(
-						"data.flexibleengine_networking_network_v2.net", "admin_state_up", "true"),
-				),
-			},
-		},
-	})
-}
+func TestAccNetworkingNetworkV2DataSource_basic(t *testing.T) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	network := fmt.Sprintf("acc_test_network-%06x", rand.Int31n(1000000))
+	cidr := fmt.Sprintf("192.168.%d.0/24", rand.Intn(200))
 
-func TestAccFlexibleEngineNetworkingNetworkV2DataSource_subnet(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		PreCheck:  func() { testAccPreCheck(t) },
 		Providers: testAccProviders,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccFlexibleEngineNetworkingNetworkV2DataSource_network,
-			},
-			{
-				Config: testAccFlexibleEngineNetworkingNetworkV2DataSource_subnet,
+				Config: testAccNetworkingNetworkV2DataSource_basic(network, cidr),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckNetworkingNetworkV2DataSourceID("data.flexibleengine_networking_network_v2.net"),
+					testAccCheckNetworkingNetworkV2DataSourceID("data.flexibleengine_networking_network_v2.net_by_name"),
+					testAccCheckNetworkingNetworkV2DataSourceID("data.flexibleengine_networking_network_v2.net_by_id"),
+					testAccCheckNetworkingNetworkV2DataSourceID("data.flexibleengine_networking_network_v2.net_by_cidr"),
 					resource.TestCheckResourceAttr(
-						"data.flexibleengine_networking_network_v2.net", "name", "flexibleengine_test_network"),
+						"data.flexibleengine_networking_network_v2.net_by_name", "name", network),
 					resource.TestCheckResourceAttr(
-						"data.flexibleengine_networking_network_v2.net", "admin_state_up", "true"),
-				),
-			},
-		},
-	})
-}
-
-func TestAccFlexibleEngineNetworkingNetworkV2DataSource_networkID(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccFlexibleEngineNetworkingNetworkV2DataSource_network,
-			},
-			{
-				Config: testAccFlexibleEngineNetworkingNetworkV2DataSource_networkID,
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckNetworkingNetworkV2DataSourceID("data.flexibleengine_networking_network_v2.net"),
+						"data.flexibleengine_networking_network_v2.net_by_id", "name", network),
 					resource.TestCheckResourceAttr(
-						"data.flexibleengine_networking_network_v2.net", "name", "flexibleengine_test_network"),
+						"data.flexibleengine_networking_network_v2.net_by_cidr", "name", network),
 					resource.TestCheckResourceAttr(
-						"data.flexibleengine_networking_network_v2.net", "admin_state_up", "true"),
+						"data.flexibleengine_networking_network_v2.net_by_name", "admin_state_up", "true"),
+					resource.TestCheckResourceAttr(
+						"data.flexibleengine_networking_network_v2.net_by_id", "admin_state_up", "true"),
+					resource.TestCheckResourceAttr(
+						"data.flexibleengine_networking_network_v2.net_by_cidr", "matching_subnet_cidr", cidr),
 				),
 			},
 		},
@@ -89,40 +58,30 @@ func testAccCheckNetworkingNetworkV2DataSourceID(n string) resource.TestCheckFun
 	}
 }
 
-const testAccFlexibleEngineNetworkingNetworkV2DataSource_network = `
+func testAccNetworkingNetworkV2DataSource_basic(name, cidr string) string {
+	return fmt.Sprintf(`
 resource "flexibleengine_networking_network_v2" "net" {
-        name = "flexibleengine_test_network"
-        admin_state_up = "true"
+  name = "%s"
+  admin_state_up = "true"
 }
 
 resource "flexibleengine_networking_subnet_v2" "subnet" {
   name = "flexibleengine_test_subnet"
-  cidr = "192.168.198.0/24"
+  cidr = "%s"
   no_gateway = true
-  network_id = "${flexibleengine_networking_network_v2.net.id}"
+  network_id = flexibleengine_networking_network_v2.net.id
 }
-`
 
-var testAccFlexibleEngineNetworkingNetworkV2DataSource_basic = fmt.Sprintf(`
-%s
-
-data "flexibleengine_networking_network_v2" "net" {
-	name = "${flexibleengine_networking_network_v2.net.name}"
+data "flexibleengine_networking_network_v2" "net_by_name" {
+	name = flexibleengine_networking_network_v2.net.name
 }
-`, testAccFlexibleEngineNetworkingNetworkV2DataSource_network)
 
-var testAccFlexibleEngineNetworkingNetworkV2DataSource_subnet = fmt.Sprintf(`
-%s
-
-data "flexibleengine_networking_network_v2" "net" {
-	matching_subnet_cidr = "${flexibleengine_networking_subnet_v2.subnet.cidr}"
+data "flexibleengine_networking_network_v2" "net_by_id" {
+	network_id = flexibleengine_networking_network_v2.net.id
 }
-`, testAccFlexibleEngineNetworkingNetworkV2DataSource_network)
 
-var testAccFlexibleEngineNetworkingNetworkV2DataSource_networkID = fmt.Sprintf(`
-%s
-
-data "flexibleengine_networking_network_v2" "net" {
-	network_id = "${flexibleengine_networking_network_v2.net.id}"
+data "flexibleengine_networking_network_v2" "net_by_cidr" {
+	matching_subnet_cidr = flexibleengine_networking_subnet_v2.subnet.cidr
 }
-`, testAccFlexibleEngineNetworkingNetworkV2DataSource_network)
+`, name, cidr)
+}
