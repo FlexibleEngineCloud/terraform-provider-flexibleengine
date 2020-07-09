b2c1be5d61a7ea99b852c2054b5b5a19f519eb1e
shichangkuo <shichangkuo90@gmail.com>
2020-07-09 09:51:19 +0800

    Add iam user and group resources and docs (#356)
    * Add identity users resource and docs
    
    * cleanup vendor files
    
    * Add identity group resource and docs
    
    * Add identity group membership resource and docs

---
+++ flexibleengine/resource_flexibleengine_identity_group_v3.go
@@ -0,0 +1,139 @@
+package flexibleengine
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/huaweicloud/golangsdk/openstack/identity/v3/groups"
+)
+
+func resourceIdentityGroupV3() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceIdentityGroupV3Create,
+		Read:   resourceIdentityGroupV3Read,
+		Update: resourceIdentityGroupV3Update,
+		Delete: resourceIdentityGroupV3Delete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"domain_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+			},
+
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+		},
+	}
+}
+
+func resourceIdentityGroupV3Create(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	identityClient, err := config.identityV3Client(GetRegion(d, config))
+	if err != nil {
+		return fmt.Errorf("Error creating FlexibleEngine identity client: %s", err)
+	}
+
+	createOpts := groups.CreateOpts{
+		Name:        d.Get("name").(string),
+		Description: d.Get("description").(string),
+		DomainID:    d.Get("domain_id").(string),
+	}
+
+	log.Printf("[DEBUG] Create Options: %#v", createOpts)
+
+	group, err := groups.Create(identityClient, createOpts).Extract()
+	if err != nil {
+		return fmt.Errorf("Error creating FlexibleEngine Group: %s", err)
+	}
+
+	d.SetId(group.ID)
+
+	return resourceIdentityGroupV3Read(d, meta)
+}
+
+func resourceIdentityGroupV3Read(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	identityClient, err := config.identityV3Client(GetRegion(d, config))
+	if err != nil {
+		return fmt.Errorf("Error creating FlexibleEngine identity client: %s", err)
+	}
+
+	group, err := groups.Get(identityClient, d.Id()).Extract()
+	if err != nil {
+		return CheckDeleted(d, err, "group")
+	}
+
+	log.Printf("[DEBUG] Retrieved FlexibleEngine Group: %#v", group)
+
+	d.Set("domain_id", group.DomainID)
+	d.Set("description", group.Description)
+	d.Set("name", group.Name)
+
+	return nil
+}
+
+func resourceIdentityGroupV3Update(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	identityClient, err := config.identityV3Client(GetRegion(d, config))
+	if err != nil {
+		return fmt.Errorf("Error creating FlexibleEngine identity client: %s", err)
+	}
+
+	var hasChange bool
+	var updateOpts groups.UpdateOpts
+
+	if d.HasChange("description") {
+		hasChange = true
+		updateOpts.Description = d.Get("description").(string)
+	}
+
+	if d.HasChange("domain_id") {
+		hasChange = true
+		updateOpts.DomainID = d.Get("domain_id").(string)
+	}
+
+	if d.HasChange("name") {
+		hasChange = true
+		updateOpts.Name = d.Get("name").(string)
+	}
+
+	if hasChange {
+		log.Printf("[DEBUG] Update Options: %#v", updateOpts)
+	}
+
+	if hasChange {
+		_, err := groups.Update(identityClient, d.Id(), updateOpts).Extract()
+		if err != nil {
+			return fmt.Errorf("Error updating FlexibleEngine group: %s", err)
+		}
+	}
+
+	return resourceIdentityGroupV3Read(d, meta)
+}
+
+func resourceIdentityGroupV3Delete(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	identityClient, err := config.identityV3Client(GetRegion(d, config))
+	if err != nil {
+		return fmt.Errorf("Error creating FlexibleEngine identity client: %s", err)
+	}
+
+	err = groups.Delete(identityClient, d.Id()).ExtractErr()
+	if err != nil {
+		return fmt.Errorf("Error deleting FlexibleEngine group: %s", err)
+	}
+
+	return nil
+}
---
+++ website/docs/r/identity_group_membership_v3.html.markdown
@@ -0,0 +1,61 @@
+---
+layout: "flexibleengine"
+page_title: "FlexibleEngine: flexibleengine_identity_group_membership_v3"
+sidebar_current: "docs-flexibleengine-resource-identity-group-membership-v3"
+description: |-
+  Manages the membership combine User Group resource and User resource  within
+  FlexibleEngine IAM service.
+---
+
+# flexibleengine\_identity\_group_membership_v3
+
+Manages a User Group Membership resource within FlexibleEngine IAM service.
+
+Note: You _must_ have admin privileges in your FlexibleEngine cloud to use
+this resource.
+
+## Example Usage
+
+```hcl
+resource "flexibleengine_identity_group_v3" "group_1" {
+  name        = "group1"
+  description = "This is a test group"
+}
+
+resource "flexibleengine_identity_user_v3" "user_1" {
+  name     = "user1"
+  enabled  = true
+  password = "password12345!"
+}
+
+resource "flexibleengine_identity_user_v3" "user_2" {
+  name     = "user2"
+  enabled  = true
+  password = "password12345!"
+}
+
+resource "flexibleengine_identity_group_membership_v3" "membership_1" {
+  group = flexibleengine_identity_group_v3.group_1.id
+  users = [
+    flexibleengine_identity_user_v3.user_1.id,
+    flexibleengine_identity_user_v3.user_2.id
+  ]
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `group` - (Required) The group ID of this membership. 
+
+* `users` - (Required) A List of user IDs to associate to the group.
+
+## Attributes Reference
+
+The following attributes are exported:
+
+* `group` - See Argument Reference above.
+
+* `users` - See Argument Reference above.
+
