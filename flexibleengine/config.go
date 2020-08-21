65b9e2589826eaed36c46466fda7a9b48decaec7
shichangkuo <shichangkuo90@gmail.com>
2020-08-21 16:27:52 +0800

    Add sfs turbo resource and docs (#384)

--- flexibleengine/config.go
+++ flexibleengine/config.go
@@ -523,6 +523,21 @@ func (c *Config) sfsV2Client(region string) (*golangsdk.ServiceClient, error) {
 	})
 }
 
+// sfsV1Client used to sfs-turbo resource
+func (c *Config) sfsV1Client(region string) (*golangsdk.ServiceClient, error) {
+	sc, err := huaweisdk.NewSharedFileSystemV2(c.HwClient, golangsdk.EndpointOpts{
+		Region:       c.determineRegion(region),
+		Availability: c.getHwEndpointType(),
+	})
+
+	if err == nil {
+		sc.Endpoint = strings.Replace(sc.Endpoint, "sfs", "sfs-turbo", 1)
+		sc.Endpoint = strings.Replace(sc.Endpoint, "/v2/", "/v1/", 1)
+		sc.ResourceBase = sc.Endpoint
+	}
+	return sc, err
+}
+
 //computeV2HWClient used to access the v2 bms Services i.e. flavor, nic, keypair.
 func (c *Config) computeV2HWClient(region string) (*golangsdk.ServiceClient, error) {
 	return huaweisdk.NewComputeV2(c.HwClient, golangsdk.EndpointOpts{
