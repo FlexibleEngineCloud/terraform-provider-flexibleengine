28673d469f3817e9e98951ee505651dc28d8e3eb
ShiChangkuo <shichangkuo90@gmail.com>
2021-01-07 06:18:21 +0300

    support tags and update acctest for dns zone

--- flexibleengine/resource_flexibleengine_dns_zone_v2_test.go
+++ flexibleengine/resource_flexibleengine_dns_zone_v2_test.go
@@ -14,10 +14,10 @@ import (
 
 func TestAccDNSV2Zone_basic(t *testing.T) {
 	var zone zones.Zone
-	// TODO: why does back-end convert name to lowercase?
 	var zoneName = fmt.Sprintf("acpttest%s.com.", acctest.RandString(5))
+	resourceName := "flexibleengine_dns_zone_v2.zone_1"
 
-	resource.Test(t, resource.TestCase{
+	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckDNSV2ZoneDestroy,
@@ -25,9 +25,56 @@ func TestAccDNSV2Zone_basic(t *testing.T) {
 			{
 				Config: testAccDNSV2Zone_basic(zoneName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDNSV2ZoneExists("flexibleengine_dns_zone_v2.zone_1", &zone),
-					resource.TestCheckResourceAttr(
-						"flexibleengine_dns_zone_v2.zone_1", "description", "a zone"),
+					testAccCheckDNSV2ZoneExists(resourceName, &zone),
+					resource.TestCheckResourceAttr(resourceName, "name", zoneName),
+					resource.TestCheckResourceAttr(resourceName, "zone_type", "public"),
+					resource.TestCheckResourceAttr(resourceName, "description", "a zone"),
+					resource.TestCheckResourceAttr(resourceName, "email", "email1@example.com"),
+					resource.TestCheckResourceAttr(resourceName, "ttl", "300"),
+					resource.TestCheckResourceAttr(resourceName, "tags.zone_type", "public"),
+					resource.TestCheckResourceAttr(resourceName, "tags.owner", "terraform"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				Config: testAccDNSV2Zone_update(zoneName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "description", "an updated zone"),
+					resource.TestCheckResourceAttr(resourceName, "email", "email2@example.com"),
+					resource.TestCheckResourceAttr(resourceName, "ttl", "600"),
+					resource.TestCheckResourceAttr(resourceName, "tags.zone_type", "public"),
+					resource.TestCheckResourceAttr(resourceName, "tags.owner", "tf-acc"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDNSV2Zone_private(t *testing.T) {
+	var zone zones.Zone
+	var zoneName = fmt.Sprintf("acpttest%s.com.", acctest.RandString(5))
+	resourceName := "flexibleengine_dns_zone_v2.zone_1"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:     func() { testAccPreCheck(t) },
+		Providers:    testAccProviders,
+		CheckDestroy: testAccCheckDNSV2ZoneDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDNSV2Zone_private(zoneName),
+				Check: resource.ComposeTestCheckFunc(
+					testAccCheckDNSV2ZoneExists(resourceName, &zone),
+					resource.TestCheckResourceAttr(resourceName, "name", zoneName),
+					resource.TestCheckResourceAttr(resourceName, "zone_type", "private"),
+					resource.TestCheckResourceAttr(resourceName, "description", "a private zone"),
+					resource.TestCheckResourceAttr(resourceName, "email", "email@example.com"),
+					resource.TestCheckResourceAttr(resourceName, "ttl", "300"),
+					resource.TestCheckResourceAttr(resourceName, "tags.zone_type", "private"),
+					resource.TestCheckResourceAttr(resourceName, "tags.owner", "terraform"),
 				),
 			},
 		},
@@ -37,8 +84,9 @@ func TestAccDNSV2Zone_basic(t *testing.T) {
 func TestAccDNSV2Zone_readTTL(t *testing.T) {
 	var zone zones.Zone
 	var zoneName = fmt.Sprintf("acpttest%s.com.", acctest.RandString(5))
+	resourceName := "flexibleengine_dns_zone_v2.zone_1"
 
-	resource.Test(t, resource.TestCase{
+	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheck(t) },
 		Providers:    testAccProviders,
 		CheckDestroy: testAccCheckDNSV2ZoneDestroy,
@@ -46,28 +94,8 @@ func TestAccDNSV2Zone_readTTL(t *testing.T) {
 			{
 				Config: testAccDNSV2Zone_readTTL(zoneName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDNSV2ZoneExists("flexibleengine_dns_zone_v2.zone_1", &zone),
-					resource.TestMatchResourceAttr(
-						"flexibleengine_dns_zone_v2.zone_1", "ttl", regexp.MustCompile("^[0-9]+$")),
-				),
-			},
-		},
-	})
-}
-
-func TestAccDNSV2Zone_timeout(t *testing.T) {
-	var zone zones.Zone
-	var zoneName = fmt.Sprintf("acpttest%s.com.", acctest.RandString(5))
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckDNSV2ZoneDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDNSV2Zone_timeout(zoneName),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDNSV2ZoneExists("flexibleengine_dns_zone_v2.zone_1", &zone),
+					testAccCheckDNSV2ZoneExists(resourceName, &zone),
+					resource.TestMatchResourceAttr(resourceName, "ttl", regexp.MustCompile("^[0-9]+$")),
 				),
 			},
 		},
@@ -129,49 +157,65 @@ func testAccCheckDNSV2ZoneExists(n string, zone *zones.Zone) resource.TestCheckF
 
 func testAccDNSV2Zone_basic(zoneName string) string {
 	return fmt.Sprintf(`
-		resource "flexibleengine_dns_zone_v2" "zone_1" {
-			name = "%s"
-			email = "email1@example.com"
-			description = "a zone"
-			ttl = 3000
-			#type = "PRIMARY"
-		}
+resource "flexibleengine_dns_zone_v2" "zone_1" {
+  name        = "%s"
+  email       = "email1@example.com"
+  description = "a zone"
+  ttl         = 300
+
+  tags = {
+    zone_type = "public"
+    owner     = "terraform"
+  }
+}
 	`, zoneName)
 }
 
 func testAccDNSV2Zone_update(zoneName string) string {
 	return fmt.Sprintf(`
-		resource "flexibleengine_dns_zone_v2" "zone_1" {
-			name = "%s"
-			email = "email2@example.com"
-			description = "an updated zone"
-			ttl = 6000
-			#type = "PRIMARY"
-		}
+resource "flexibleengine_dns_zone_v2" "zone_1" {
+  name        = "%s"
+  email       = "email2@example.com"
+  description = "an updated zone"
+  ttl         = 600
+
+  tags = {
+    zone_type = "public"
+    owner     = "tf-acc"
+  }
+}
 	`, zoneName)
 }
 
 func testAccDNSV2Zone_readTTL(zoneName string) string {
 	return fmt.Sprintf(`
-		resource "flexibleengine_dns_zone_v2" "zone_1" {
-			name = "%s"
-			email = "email1@example.com"
-		}
+resource "flexibleengine_dns_zone_v2" "zone_1" {
+  name  = "%s"
+  email = "email1@example.com"
+}
 	`, zoneName)
 }
 
-func testAccDNSV2Zone_timeout(zoneName string) string {
+func testAccDNSV2Zone_private(rName string) string {
 	return fmt.Sprintf(`
-		resource "flexibleengine_dns_zone_v2" "zone_1" {
-			name = "%s"
-			email = "email@example.com"
-			ttl = 3000
+resource "flexibleengine_vpc_v1" "vpc_1" {
+  name = "%s"
+  cidr = "192.168.0.0/16"
+}
 
-			timeouts {
-				create = "5m"
-				update = "5m"
-				delete = "5m"
-			}
-		}
-	`, zoneName)
+resource "flexibleengine_dns_zone_v2" "zone_1" {
+  name        = "%s"
+  email       = "email@example.com"
+  description = "a private zone"
+  zone_type   = "private"
+
+  router {
+    router_id = flexibleengine_vpc_v1.vpc_1.id
+  }
+  tags = {
+    zone_type = "private"
+    owner     = "terraform"
+  }
+}
+	`, rName, rName)
 }
