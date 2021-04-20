a2bda96cd94a16e3f1e5ae58088e9c6c8e0dedf2
Benjamin Geneze <benjamin.geneze@orange.com>
2021-04-20 23:24:44 +0200

    Add vpcep endpoints data source

---
+++ flexibleengine/data_source_flexibleengine_vpcep_endpoints.go
@@ -0,0 +1,184 @@
+package flexibleengine
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/huaweicloud/golangsdk/openstack/vpcep/v1/endpoints"
+)
+
+func dataSourceVPCEPEndpoints() *schema.Resource {
+	return &schema.Resource{
+		Read: dataSourceVpcepEndpointsRead,
+
+		Schema: map[string]*schema.Schema{
+			"vpc_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"service_name": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"endpoint_id": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"endpoints": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"status": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"service_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"service_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"service_type": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"vpc_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"network_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"ip_address": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"enable_dns": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"enable_whitelist": {
+							Type:     schema.TypeBool,
+							Computed: true,
+						},
+						"whitelist": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem:     &schema.Schema{Type: schema.TypeString},
+						},
+						"packet_id": {
+							Type:     schema.TypeInt,
+							Computed: true,
+						},
+						"private_domain_name": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"tags": {
+							Type:     schema.TypeList,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"key": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"value": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+						"project_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"created_at": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"updated_at": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceVpcepEndpointsRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	region := GetRegion(d, config)
+	vpcepClient, err := config.vpcepV1Client(region)
+	if err != nil {
+		return fmt.Errorf("Error creating FlexibleEngine VPC endpoint client: %s", err)
+	}
+
+	listOpts := endpoints.ListOpts{
+		ServiceName: d.Get("service_name").(string),
+		VPCID:       d.Get("vpc_id").(string),
+		ID:          d.Get("endpoint_id").(string),
+	}
+
+	allEndpoints, err := endpoints.List(vpcepClient, listOpts)
+	if err != nil {
+		return fmt.Errorf("Unable to retrieve vpc endpoints: %s", err)
+	}
+
+	if len(allEndpoints) < 1 {
+		return fmt.Errorf("Your query returned no results. " +
+			"Please change your search criteria and try again.")
+	}
+
+	d.SetId(allEndpoints[0].ID)
+	endpoints := make([]map[string]interface{}, len(allEndpoints))
+	for i, v := range allEndpoints {
+		var tag []map[string]interface{}
+		for _, tagContent := range v.Tags {
+			mapping := map[string]interface{}{
+				"key":   tagContent.Key,
+				"value": tagContent.Value,
+			}
+			tag = append(tag, mapping)
+		}
+		privateDomainName := ""
+		if len(v.DNSNames) > 0 {
+			privateDomainName = v.DNSNames[0]
+		}
+		endpoints[i] = map[string]interface{}{
+			"id":                  v.ID,
+			"status":              v.Status,
+			"service_id":          v.ServiceID,
+			"service_name":        v.ServiceName,
+			"service_type":        v.ServiceType,
+			"vpc_id":              v.VpcID,
+			"network_id":          v.SubnetID,
+			"ip_address":          v.IPAddr,
+			"enable_dns":          v.EnableDNS,
+			"enable_whitelist":    v.EnableWhitelist,
+			"whitelist":           v.Whitelist,
+			"packet_id":           v.MarkerID,
+			"private_domain_name": privateDomainName,
+			"tags":                tag,
+			"project_id":          v.ProjectID,
+			"created_at":          v.Created,
+			"updated_at":          v.Updated,
+		}
+	}
+	if err := d.Set("endpoints", endpoints); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ flexibleengine/data_source_flexibleengine_vpcep_endpoints_test.go
@@ -0,0 +1,129 @@
+package flexibleengine
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
+)
+
+func TestAccVPCEPEndpointsDataSourceBasic(t *testing.T) {
+
+	rName := fmt.Sprintf("acc-test-%s", acctest.RandString(4))
+	endpointByNameResourceName := "data.flexibleengine_vpcep_endpoints.by_name"
+	endpointByEndpointIdResourceName := "data.flexibleengine_vpcep_endpoints.by_endpoint_id"
+	endpointByVpcIdResourceName := "data.flexibleengine_vpcep_endpoints.by_vpc_id"
+
+	fmt.Sprintf(testAccVPCEPEndpointsDataSourceBasic(rName))
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccVPCEPEndpointsDataSourceBasic(rName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttrSet(endpointByNameResourceName, "endpoints.0.vpc_id"),
+					resource.TestCheckResourceAttrSet(endpointByNameResourceName, "endpoints.0.service_name"),
+					resource.TestCheckResourceAttrSet(endpointByNameResourceName, "endpoints.0.service_id"),
+					resource.TestCheckResourceAttrSet(endpointByEndpointIdResourceName, "endpoints.0.vpc_id"),
+					resource.TestCheckResourceAttrSet(endpointByEndpointIdResourceName, "endpoints.0.service_name"),
+					resource.TestCheckResourceAttrSet(endpointByEndpointIdResourceName, "endpoints.0.service_id"),
+					resource.TestCheckResourceAttrSet(endpointByVpcIdResourceName, "endpoints.0.vpc_id"),
+					resource.TestCheckResourceAttrSet(endpointByVpcIdResourceName, "endpoints.0.service_name"),
+					resource.TestCheckResourceAttrSet(endpointByVpcIdResourceName, "endpoints.0.service_id"),
+				),
+			},
+		},
+	})
+}
+
+func testAccVPCEndpointDataSourcePrecondition(rName string) string {
+	return fmt.Sprintf(`
+resource flexibleengine_networking_network_v2 test {
+  name           = "%[1]s"
+  admin_state_up = "true"
+}
+
+resource flexibleengine_networking_subnet_v2 test {
+  name            = "%[1]s"
+  cidr            = "192.168.0.0/24"
+  gateway_ip      = "192.168.0.1"
+  network_id      = flexibleengine_networking_network_v2.test.id
+}
+
+resource flexibleengine_vpc_v1 test {
+  name = "%[1]s"
+  cidr = "192.168.0.0/24"
+  tags = {
+    owner = "terraform-test"
+  }
+}
+
+resource flexibleengine_networking_router_interface_v2 test { 
+  router_id = flexibleengine_vpc_v1.test.id 
+  subnet_id = flexibleengine_networking_subnet_v2.test.id 
+} 
+
+resource flexibleengine_compute_instance_v2 test {
+  name = "%[1]s"
+  security_groups = ["default"]
+  availability_zone = "%[2]s"
+
+  network {
+    uuid = flexibleengine_networking_network_v2.test.id
+  }
+
+  tags = {
+    owner   = "terraform-test"
+  }
+  depends_on = [ flexibleengine_networking_router_interface_v2.test ]
+}
+
+resource "flexibleengine_vpcep_service" "test" {
+  name        = "%[1]s"
+  server_type = "VM"
+  vpc_id      = flexibleengine_vpc_v1.test.id
+  port_id     = flexibleengine_compute_instance_v2.test.network[0].port
+  approval    = false
+
+  port_mapping {
+    service_port  = 22
+    terminal_port = 22
+  }
+  tags = {
+    owner = "terraform-test"
+  }
+}
+
+resource flexibleengine_vpcep_endpoint test {
+  service_id = flexibleengine_vpcep_service.test.id
+  vpc_id = flexibleengine_vpc_v1.test.id
+  network_id = flexibleengine_networking_network_v2.test.id
+  tags = {
+    owner = "terraform-test"
+  }
+}
+
+`, rName, OS_AVAILABILITY_ZONE)
+}
+
+func testAccVPCEPEndpointsDataSourceBasic(rName string) string {
+	return fmt.Sprintf(`
+%s
+
+data flexibleengine_vpcep_endpoints by_name {
+   service_name = flexibleengine_vpcep_endpoint.test.service_name
+}
+
+data flexibleengine_vpcep_endpoints by_endpoint_id {
+   endpoint_id = flexibleengine_vpcep_endpoint.test.id
+}
+
+data flexibleengine_vpcep_endpoints by_vpc_id {
+   vpc_id = flexibleengine_vpcep_endpoint.test.vpc_id
+}
+
+`, testAccVPCEndpointDataSourcePrecondition(rName))
+}
