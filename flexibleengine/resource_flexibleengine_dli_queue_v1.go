9b6978f2d9233e794a7ab7f5c6d2848175a2ade4
hua ming <hmbig2@163.com>
2021-07-27 16:26:24 +0800

    feat: add dli queues support (#588)

---
+++ flexibleengine/resource_flexibleengine_dli_queue_v1.go
@@ -0,0 +1,203 @@
+package flexibleengine
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+
+	"github.com/hashicorp/terraform-plugin-sdk/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/helper/validation"
+
+	"github.com/huaweicloud/golangsdk/openstack/common/tags"
+	"github.com/huaweicloud/golangsdk/openstack/dli/v1/queues"
+)
+
+var regexp4Name = regexp.MustCompile(`^[a-z0-9_]{1,128}$`)
+
+func ResourceDliQueueV1() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceDliQueueCreate,
+		Read:   resourceDliQueueV1Read,
+		Delete: resourceDliQueueV1Delete,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+				ValidateFunc: func(val interface{}, key string) (warns []string, errs []error) {
+					v := val.(string)
+					if !regexp4Name.MatchString(v) {
+						errs = append(errs, fmt.Errorf("%q can contain only digits, lower letters, and underscores (_) ", key))
+					}
+					return
+				},
+			},
+
+			"queue_type": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				Default:      "sql",
+				ValidateFunc: validation.StringInSlice([]string{"sql", "general"}, false),
+			},
+
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+			},
+
+			"cu_count": {
+				Type:         schema.TypeInt,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.IntInSlice([]int{16, 64, 256}),
+			},
+
+			"resource_mode": {
+				Type:         schema.TypeInt,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: validation.IntInSlice([]int{0, 1}),
+			},
+
+			"tags": {
+				Type: schema.TypeMap,
+				Elem: &schema.Schema{
+					Type: schema.TypeString,
+				},
+				Optional: true,
+				ForceNew: true,
+			},
+
+			"create_time": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceDliQueueCreate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	region := GetRegion(d, config)
+
+	dliClient, err := config.DliV1Client(region)
+	if err != nil {
+		return fmt.Errorf("creating dli client failed: %s", err)
+	}
+
+	queueName := d.Get("name").(string)
+
+	log.Printf("[DEBUG] create dli queues queueName: %s", queueName)
+	createOpts := queues.CreateOpts{
+		QueueName:    queueName,
+		QueueType:    d.Get("queue_type").(string),
+		Description:  d.Get("description").(string),
+		CuCount:      d.Get("cu_count").(int),
+		ResourceMode: d.Get("resource_mode").(int),
+		Tags:         assembleTagsFromRecource("tags", d),
+	}
+
+	log.Printf("[DEBUG] create dli queues using paramaters: %+v", createOpts)
+	createResult := queues.Create(dliClient, createOpts)
+	if createResult.Err != nil {
+		return fmt.Errorf("create dli queues failed: %s", createResult.Err)
+	}
+
+	//query queue detail,trriger read to refresh the state
+	d.SetId(queueName)
+
+	return resourceDliQueueV1Read(d, meta)
+}
+
+func assembleTagsFromRecource(key string, d *schema.ResourceData) []tags.ResourceTag {
+	if v, ok := d.GetOk(key); ok {
+		tagRaw := v.(map[string]interface{})
+		taglist := expandResourceTags(tagRaw)
+		return taglist
+	}
+	return nil
+
+}
+
+func resourceDliQueueV1Read(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	region := GetRegion(d, config)
+
+	dliClient, err := config.DliV1Client(region)
+
+	if err != nil {
+		return fmt.Errorf("creating dli client failed, err=%s", err)
+	}
+
+	queueName := d.Get("name").(string)
+
+	queryOpts := queues.ListOpts{
+		QueueType: d.Get("queue_type").(string),
+	}
+
+	log.Printf("[DEBUG] create dli queues using paramaters: %+v", queryOpts)
+
+	queryAllResult := queues.List(dliClient, queryOpts)
+	if queryAllResult.Err != nil {
+		return fmt.Errorf("query queues failed: %s", queryAllResult.Err)
+	}
+
+	//filter by queue_name
+	queueDetail, err := filterByQueueName(queryAllResult.Body, queueName)
+	if err != nil {
+		return err
+	}
+
+	if queueDetail != nil {
+		log.Printf("[DEBUG]The detail of queue from SDK:%+v", queueDetail)
+
+		d.Set("name", queueDetail.QueueName)
+		d.Set("queue_type", queueDetail.QueueType)
+		d.Set("description", queueDetail.Description)
+		d.Set("cu_count", queueDetail.CuCount)
+		d.Set("resource_mode", queueDetail.ResourceMode)
+	}
+
+	return nil
+}
+
+func filterByQueueName(body interface{}, queueName string) (r *queues.Queue, err error) {
+	if queueList, ok := body.(*queues.ListResult); ok {
+		log.Printf("[DEBUG]The list of queue from SDK:%+v", queueList)
+
+		for _, v := range queueList.Queues {
+			if v.QueueName == queueName {
+				return &v, nil
+			}
+		}
+		return nil, nil
+
+	} else {
+		return nil, fmt.Errorf("sdk-client response type is wrong, expect type:*queues.ListResult,acutal Type:%T",
+			body)
+	}
+
+}
+
+func resourceDliQueueV1Delete(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	region := GetRegion(d, config)
+
+	client, err := config.DliV1Client(region)
+	if err != nil {
+		return fmt.Errorf("creating dli client failed, err=%s", err)
+	}
+
+	queueName := d.Get("name").(string)
+	log.Printf("[DEBUG] Deleting dli Queue %q", d.Id())
+
+	result := queues.Delete(client, queueName)
+	if result.Err != nil {
+		return fmt.Errorf("error deleting dli Queue %q, err=%s", d.Id(), result.Err)
+	}
+
+	return nil
+}
