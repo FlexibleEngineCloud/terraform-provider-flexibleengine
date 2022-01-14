eb5925caacd762c0a97de6969f56cda2fc3f39ea
ShiChangkuo <shichangkuo90@gmail.com>
2022-01-14 09:52:12 +0300

    feat: add dms kafka instance resource

---
+++ docs/resources/dms_kafka_instance.md
@@ -0,0 +1,156 @@
+---
+subcategory: "Distributed Message Service (DMS)"
+---
+
+# flexibleengine_dms_kafka_instance
+
+Manage a DMS Kafka instance resources within FlexibleEngine.
+
+## Example Usage
+
+```hcl
+variable vpc_id {}
+variable subnet_id {}
+variable security_group_id {}
+
+data "flexibleengine_dms_product" "test" {
+  bandwidth = "100MB"
+}
+
+resource "flexibleengine_dms_kafka_instance" "product_1" {
+  name               = "instance_1"
+  engine_version     = "2.3.0"
+  bandwidth          = "100MB"
+  availability_zones = data.flexibleengine_dms_product.product_1.availability_zones
+  product_id         = data.flexibleengine_dms_product.test.id
+  storage_space      = data.flexibleengine_dms_product.test.storage_space
+  storage_spec_code  = "dms.physical.storage.ultra"
+
+  vpc_id            = var.vpc_id
+  network_id        = var.subnet_id
+  security_group_id = var.security_group_id
+
+  access_user = "user"
+  password    = "Kafkatest@123"
+}
+```
+
+## Argument Reference
+
+The following arguments are supported:
+
+* `region` - (Optional, String, ForceNew) Specifies the region in which to create the DMS Kafka instance resource.
+  If omitted, the provider-level region will be used. Changing this creates a new instance resource.
+
+* `name` - (Required, String) Specifies the name of the DMS Kafka instance. An instance name starts with a letter,
+  consists of 4 to 64 characters, and supports only letters, digits, hyphens (-) and underscores (_).
+
+* `bandwidth` - (Required, String, ForceNew) The baseline bandwidth of a Kafka instance, that is, the maximum amount of
+  data transferred per unit time. The valid values are **100MB**, **300MB**, **600MB** and **1200MB**.
+  Changing this creates a new instance resource.
+
+* `product_id` - (Required, String, ForceNew) Specifies a product ID. You can get the value from id of
+  [flexibleengine_dms_product](https://registry.terraform.io/providers/FlexibleEngineCloud/flexibleengine/latest/docs/data-sources/dms_product)
+  data source. Changing this creates a new instance resource.
+
+* `storage_space` - (Required, Int, ForceNew) Specifies the message storage capacity, the unit is GB. Value range:
+  + When bandwidth is **100MB**: 600–90,000 GB
+  + When bandwidth is **300MB**: 1,200–90,000 GB
+  + When bandwidth is **600MB**: 2,400–90,000 GB
+  + When bandwidth is **1,200MB**: 4,800–90,000 GB
+
+  Changing this creates a new instance resource.
+
+* `availability_zones` - (Required, List, ForceNew) The names of the AZ where the Kafka instance resides.
+  Changing this creates a new instance resource.
+
+  -> **NOTE:** Deploy one availability zone or at least three availability zones. Do not select two availability zones.
+  Deploy to more availability zones, the better the reliability and SLA coverage.
+
+* `vpc_id` - (Required, String, ForceNew) Specifies the ID of a VPC.
+  Changing this creates a new instance resource.
+
+* `network_id` - (Required, String, ForceNew) Specifies the ID of a subnet.
+  Changing this creates a new instance resource.
+
+* `security_group_id` - (Required, String) Specifies the ID of a security group.
+
+* `description` - (Optional, String) Specifies the description of the DMS Kafka instance.
+  It is a character string containing not more than 1,024 characters.
+
+* `engine_version` - (Optional, String, ForceNew) Specifies the version of the Kafka engine. Valid values are "1.1.0"
+  and "2.3.0". Defaults to **2.3.0**. Changing this creates a new instance resource.
+
+* `storage_spec_code` - (Optional, String, ForceNew) Specifies the storage I/O specification. Value range:
+  + When bandwidth is **100MB**: dms.physical.storage.high or dms.physical.storage.ultra
+  + When bandwidth is **300MB**: dms.physical.storage.high or dms.physical.storage.ultra
+  + When bandwidth is **600MB**: dms.physical.storage.ultra
+  + When bandwidth is **1,200MB**: dms.physical.storage.ultra
+
+  Defaults to **dms.physical.storage.ultra**. Changing this creates a new instance resource.
+
+* `access_user` - (Optional, String, ForceNew) Specifies a username who can accesse the instance with
+  SASL authentication. A username consists of 4 to 64 characters and supports only letters, digits, and hyphens (-).
+  Changing this creates a new instance resource.
+
+* `password` - (Optional, String, ForceNew) Specifies the password of the access user. A password must meet the
+  following complexity requirements: Must be 8 to 32 characters long. Must contain at least 2 of the following character
+  types: lowercase letters, uppercase letters, digits, and special characters (`~!@#$%^&*()-_=+\\|[{}]:'",<.>/?).
+  Changing this creates a new instance resource.
+
+  -> **NOTE:** If `access_user` and `password` are specified, Kafka SASL_SSL will be automatically enabled.
+
+* `maintain_begin` - (Optional, String) Specifies the time at which a maintenance time window starts. Format: HH:mm:ss.
+  The start time must be set to 22:00:00, 02:00:00, 06:00:00, 10:00:00, 14:00:00, or 18:00:00.
+  The system automatically allocates the default start time 02:00:00.
+
+* `maintain_end` - (Optional, String) Specifies the time at which a maintenance time window ends. Format: HH:mm:ss.
+  The end time is four hours later than the start time. For example, if the start time is 22:00:00, the end time is
+  02:00:00. The system automatically allocates the default end time 06:00:00.
+
+  -> **NOTE:**  The start time and end time of a maintenance time window must be set in pairs.
+
+## Attributes Reference
+
+In addition to all arguments above, the following attributes are exported:
+
+* `id` - Specifies a resource ID in UUID format.
+* `status` - Indicates the status of the DMS Kafka instance.
+* `engine` - Indicates the message engine, the value is "kafka".
+* `engine_type` - Indicates the DMS Kafka instance type, the value is "cluster".
+* `product_spec_code` - Indicates the DMS Kafka instance specification.
+* `partition_num` - Indicates the maximum number of topics in the DMS Kafka instance.
+* `used_storage_space` - Indicates the used message storage space. Unit: GB
+* `vpc_name` - Indicates the name of a vpc.
+* `subnet_name` - Indicates the name of a subnet.
+* `security_group_name` - Indicates the name of a security group.
+* `node_num` - Indicates the count of ECS instances.
+* `connect_address` - Indicates the IP addresses of the DMS Kafka instance.
+* `port` - Indicates the port number of the DMS Kafka instance.
+* `ssl_enable` - Indicates whether the Kafka SASL_SSL is enabled.
+* `created_at` - Indicates the creation time of the DMS Kafka instance.
+
+## Import
+
+DMS Kafka instance can be imported using the instance id, e.g.
+
+```
+ $ terraform import flexibleengine_dms_kafka_instance.instance_1 8d3c7938-dc47-4937-a30f-c80de381c5e3
+```
+
+Note that the imported state may not be identical to your resource definition, because of `access_user` and `password`
+are missing from the API response due to security reason. It is generally recommended running `terraform plan` after
+importing a DMS Kafka instance. You can then decide if changes should be applied to the instance, or the resource
+definition should be updated to align with the instance. Also you can ignore changes as below.
+
+```
+resource "flexibleengine_dms_kafka_instance" "instance_1" {
+    ...
+
+  lifecycle {
+    ignore_changes = [
+      access_user, password,
+    ]
+  }
+}
+```
---
+++ flexibleengine/resource_flexibleengine_dms_kafka_instance.go
@@ -0,0 +1,405 @@
+package flexibleengine
+
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"time"
+
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	"github.com/chnsz/golangsdk"
+	"github.com/chnsz/golangsdk/openstack/dms/v1/instances"
+	"github.com/huaweicloud/terraform-provider-huaweicloud/huaweicloud/utils"
+)
+
+const (
+	ssdSpecCode = "dms.physical.storage.ultra"
+	sasSpecCode = "dms.physical.storage.high"
+)
+
+func resourceDmsKafkaInstances() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceDmsKafkaInstancesCreate,
+		Read:   resourceDmsKafkaInstancesRead,
+		Update: resourceDmsKafkaInstancesUpdate,
+		Delete: resourceDmsKafkaInstancesDelete,
+		Importer: &schema.ResourceImporter{
+			State: schema.ImportStatePassthrough,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"region": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Computed: true,
+				ForceNew: true,
+			},
+			"name": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"description": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"engine_version": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Default:  "2.3.0",
+			},
+
+			"bandwidth": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice(
+					[]string{"100MB", "300MB", "600MB", "1200MB"},
+					false,
+				),
+			},
+			"product_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"storage_space": {
+				Type:     schema.TypeInt,
+				Required: true,
+				ForceNew: true,
+			},
+			"storage_spec_code": {
+				Type:     schema.TypeString,
+				Optional: true,
+				ForceNew: true,
+				Default:  ssdSpecCode,
+				ValidateFunc: validation.StringInSlice(
+					[]string{ssdSpecCode, sasSpecCode},
+					false,
+				),
+			},
+
+			"vpc_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"network_id": {
+				Type:     schema.TypeString,
+				Required: true,
+				ForceNew: true,
+			},
+			"security_group_id": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"availability_zones": {
+				Type:     schema.TypeList,
+				Required: true,
+				ForceNew: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+
+			"access_user": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				RequiredWith: []string{"password"},
+			},
+			"password": {
+				Type:         schema.TypeString,
+				Sensitive:    true,
+				Optional:     true,
+				ForceNew:     true,
+				RequiredWith: []string{"access_user"},
+			},
+			"maintain_begin": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				RequiredWith: []string{"maintain_end"},
+			},
+			"maintain_end": {
+				Type:         schema.TypeString,
+				Optional:     true,
+				Computed:     true,
+				RequiredWith: []string{"maintain_begin"},
+			},
+
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"engine": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"engine_type": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"product_spec_code": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"partition_num": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"vpc_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"subnet_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"security_group_name": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"node_num": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"connect_address": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"port": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"ssl_enable": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"used_storage_space": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"created_at": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+		},
+	}
+}
+
+func resourceDmsKafkaInstancesCreate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	dmsV1Client, err := config.DmsV1Client(GetRegion(d, config))
+	if err != nil {
+		return fmt.Errorf("Error creating FlexibleEngine DMS instance client: %s", err)
+	}
+
+	sslEnable := false
+	if d.Get("access_user").(string) != "" || d.Get("password").(string) != "" {
+		sslEnable = true
+	}
+	createOpts := &instances.CreateOps{
+		Engine:          "kafka",
+		EngineVersion:   d.Get("engine_version").(string),
+		Name:            d.Get("name").(string),
+		Description:     d.Get("description").(string),
+		Specification:   d.Get("bandwidth").(string),
+		ProductID:       d.Get("product_id").(string),
+		StorageSpace:    d.Get("storage_space").(int),
+		StorageSpecCode: d.Get("storage_spec_code").(string),
+		VPCID:           d.Get("vpc_id").(string),
+		SubnetID:        d.Get("network_id").(string),
+		SecurityGroupID: d.Get("security_group_id").(string),
+		AvailableZones:  utils.ExpandToStringList(d.Get("availability_zones").([]interface{})),
+		MaintainBegin:   d.Get("maintain_begin").(string),
+		MaintainEnd:     d.Get("maintain_end").(string),
+
+		AccessUser: d.Get("access_user").(string),
+		SslEnable:  sslEnable,
+	}
+
+	log.Printf("[DEBUG] Create Options: %#v", createOpts)
+	// Add password here so it wouldn't go in the above log entry
+	createOpts.Password = d.Get("password").(string)
+
+	v, err := instances.Create(dmsV1Client, createOpts).Extract()
+	if err != nil {
+		return fmt.Errorf("Error creating FlexibleEngine DMS instance: %s", err)
+	}
+	log.Printf("[INFO] instance ID: %s", v.InstanceID)
+
+	stateConf := &resource.StateChangeConf{
+		Pending:      []string{"CREATING"},
+		Target:       []string{"RUNNING"},
+		Refresh:      dmsKafkaInstancesStateRefreshFunc(dmsV1Client, v.InstanceID),
+		Timeout:      d.Timeout(schema.TimeoutCreate),
+		Delay:        120 * time.Second,
+		PollInterval: 15 * time.Second,
+	}
+	_, err = stateConf.WaitForState()
+	if err != nil {
+		return fmt.Errorf(
+			"Error waiting for instance (%s) to become ready: %s",
+			v.InstanceID, err)
+	}
+
+	// Store the instance ID now
+	d.SetId(v.InstanceID)
+
+	return resourceDmsKafkaInstancesRead(d, meta)
+}
+
+func resourceDmsKafkaInstancesRead(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	region := GetRegion(d, config)
+	dmsV1Client, err := config.DmsV1Client(region)
+	if err != nil {
+		return fmt.Errorf("Error creating FlexibleEngine DMS instance client: %s", err)
+	}
+
+	v, err := instances.Get(dmsV1Client, d.Id()).Extract()
+	if err != nil {
+		return CheckDeleted(d, err, "DMS instance")
+	}
+
+	log.Printf("[DEBUG] Dms instance %s: %+v", d.Id(), v)
+
+	partitionNum, _ := strconv.Atoi(v.PartitionNum)
+
+	var mErr *multierror.Error
+	mErr = multierror.Append(err,
+		d.Set("region", region),
+		d.Set("availability_zones", v.AvailableZones),
+		d.Set("status", v.Status),
+		d.Set("name", v.Name),
+		d.Set("description", v.Description),
+		d.Set("engine", v.Engine),
+		d.Set("engine_version", v.EngineVersion),
+		d.Set("engine_type", v.Type),
+		d.Set("bandwidth", v.Specification),
+		d.Set("partition_num", partitionNum),
+		d.Set("product_id", v.ProductID),
+		d.Set("product_spec_code", v.ResourceSpecCode),
+		d.Set("storage_spec_code", v.StorageSpecCode),
+		d.Set("storage_space", v.TotalStorageSpace),
+		d.Set("used_storage_space", v.UsedStorageSpace),
+		d.Set("vpc_id", v.VPCID),
+		d.Set("vpc_name", v.VPCName),
+		d.Set("network_id", v.SubnetID),
+		d.Set("subnet_name", v.SubnetName),
+		d.Set("security_group_id", v.SecurityGroupID),
+		d.Set("security_group_name", v.SecurityGroupName),
+		d.Set("node_num", v.Nodeum),
+		d.Set("connect_address", v.ConnectAddress),
+		d.Set("port", v.Port),
+		d.Set("maintain_begin", v.MaintainBegin),
+		d.Set("maintain_end", v.MaintainEnd),
+		d.Set("ssl_enable", v.SslEnable),
+		d.Set("created_at", setResourceTimestamp(v.CreatedAt)),
+	)
+
+	if mErr.ErrorOrNil() != nil {
+		return fmt.Errorf("Error setting DMS product attributes: %s", mErr)
+	}
+
+	return nil
+}
+
+func setResourceTimestamp(stamp string) string {
+	mSeconds, err := strconv.ParseInt(stamp, 10, 64)
+	if err != nil {
+		log.Printf("[WARN] failed to convert string %s to int64: %s", stamp, err)
+		return ""
+	}
+	stampTime := time.Unix(mSeconds/1000, 0)
+	return stampTime.Format(time.RFC3339)
+}
+
+func resourceDmsKafkaInstancesUpdate(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	dmsV1Client, err := config.DmsV1Client(GetRegion(d, config))
+	if err != nil {
+		return fmt.Errorf("Error updating FlexibleEngine DMS instance client: %s", err)
+	}
+
+	//lintignore:R019
+	if d.HasChanges("name", "description", "maintain_begin", "maintain_end", "security_group_id") {
+		var updateOpts instances.UpdateOpts
+		if d.HasChange("name") {
+			updateOpts.Name = d.Get("name").(string)
+		}
+		if d.HasChange("description") {
+			description := d.Get("description").(string)
+			updateOpts.Description = &description
+		}
+		if d.HasChange("maintain_begin") {
+			updateOpts.MaintainBegin = d.Get("maintain_begin").(string)
+		}
+		if d.HasChange("maintain_end") {
+			updateOpts.MaintainEnd = d.Get("maintain_end").(string)
+		}
+		if d.HasChange("security_group_id") {
+			updateOpts.SecurityGroupID = d.Get("security_group_id").(string)
+		}
+
+		err = instances.Update(dmsV1Client, d.Id(), updateOpts).Err
+		if err != nil {
+			return fmt.Errorf("Error updating FlexibleEngine Dms Instance: %s", err)
+		}
+	}
+
+	return resourceDmsKafkaInstancesRead(d, meta)
+}
+
+func resourceDmsKafkaInstancesDelete(d *schema.ResourceData, meta interface{}) error {
+	config := meta.(*Config)
+	dmsV1Client, err := config.DmsV1Client(GetRegion(d, config))
+	if err != nil {
+		return fmt.Errorf("Error creating FlexibleEngine DMS instance client: %s", err)
+	}
+
+	err = instances.Delete(dmsV1Client, d.Id()).ExtractErr()
+	if err != nil {
+		return fmt.Errorf("Error deleting FlexibleEngine instance: %s", err)
+	}
+
+	// Wait for the instance to delete before moving on.
+	log.Printf("[DEBUG] Waiting for instance (%s) to delete", d.Id())
+
+	stateConf := &resource.StateChangeConf{
+		Pending:    []string{"DELETING", "RUNNING"},
+		Target:     []string{"DELETED"},
+		Refresh:    dmsKafkaInstancesStateRefreshFunc(dmsV1Client, d.Id()),
+		Timeout:    d.Timeout(schema.TimeoutDelete),
+		Delay:      10 * time.Second,
+		MinTimeout: 3 * time.Second,
+	}
+
+	_, err = stateConf.WaitForState()
+	if err != nil {
+		return fmt.Errorf(
+			"Error waiting for instance (%s) to delete: %s",
+			d.Id(), err)
+	}
+
+	log.Printf("[DEBUG] Dms instance %s deactivated.", d.Id())
+	d.SetId("")
+	return nil
+}
+
+func dmsKafkaInstancesStateRefreshFunc(client *golangsdk.ServiceClient, instanceID string) resource.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		v, err := instances.Get(client, instanceID).Extract()
+		if err != nil {
+			if _, ok := err.(golangsdk.ErrDefault404); ok {
+				return v, "DELETED", nil
+			}
+			return nil, "", err
+		}
+
+		return v, v.Status, nil
+	}
+}
