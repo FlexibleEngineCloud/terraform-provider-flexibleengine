e97b40b8664f12fc995b384f100baf52669cdae2
shichangkuo <shichangkuo90@gmail.com>
2022-12-19 15:45:23 +0800

    chore(vip): use ip_version instead of subnet_id (#868)
    * chore(vip): use ip_version instead of subnet_id
    
    * chore: update docs and acc test for vip associate

--- docs/resources/networking_vip_v2.md
+++ docs/resources/networking_vip_v2.md
@@ -11,31 +11,26 @@ Manages a V2 vip resource within FlexibleEngine.
 ## Example Usage
 
 ```hcl
-resource "flexibleengine_networking_network_v2" "network_1" {
-  name           = "network_1"
-  admin_state_up = "true"
+variable vpc_name{}
+variable vpc_cidr{}
+variable subnet_name{}
+variable subnet_cidr{}
+variable subnet_gateway_ip{}
+
+resource "flexibleengine_vpc_v1" "vpc_1" {
+  name = var.vpc_name
+  cidr = var.vpc_cidr
 }
 
-resource "flexibleengine_networking_subnet_v2" "subnet_1" {
-  name       = "subnet_1"
-  cidr       = "192.168.199.0/24"
-  ip_version = 4
-  network_id = flexibleengine_networking_network_v2.network_1.id
-}
-
-resource "flexibleengine_networking_router_interface_v2" "router_interface_1" {
-  router_id = flexibleengine_networking_router_v2.router_1.id
-  subnet_id = flexibleengine_networking_subnet_v2.subnet_1.id
-}
-
-resource "flexibleengine_networking_router_v2" "router_1" {
-  name             = "router_1"
-  external_gateway = "0a2228f2-7f8a-45f1-8e09-9039e1d09975"
+resource "flexibleengine_vpc_subnet_v1" "subnet_1" {
+  name       = var.subnet_name
+  cidr       = var.subnet_cidr
+  gateway_ip = var.subnet_gateway_ip
+  vpc_id     = flexibleengine_vpc_v1.vpc_1.id
 }
 
 resource "flexibleengine_networking_vip_v2" "vip_1" {
-  network_id = flexibleengine_networking_network_v2.network_1.id
-  subnet_id  = flexibleengine_networking_subnet_v2.subnet_1.id
+  network_id = flexibleengine_vpc_subnet_v1.subnet_1.id
 }
 ```
 
@@ -43,27 +38,43 @@ resource "flexibleengine_networking_vip_v2" "vip_1" {
 
 The following arguments are supported:
 
-* `network_id` - (Required) The ID of the network to attach the vip to.
-    Changing this creates a new vip.
+* `region` - (Optional, String, ForceNew) Specifies the region in which to create the VIP.
+  If omitted, the provider-level region will be used. Changing this will create a new VIP resource.
 
-* `subnet_id` - (Required) Subnet in which to allocate IP address for this vip.
-    Changing this creates a new vip.
+* `network_id` - (Required, String, ForceNew) Specifies the *ID* of the VPC subnet to which the VIP belongs.
+  Changing this will create a new VIP resource.
 
-* `ip_address` - (Optional) IP address desired in the subnet for this vip.
-    If you don't specify `ip_address`, an available IP address from
-    the specified subnet will be allocated to this vip.
+* `ip_version` - (Optional, Int, ForceNew) Specifies the IP version, either `4` (default) or `6`.
+  Changing this will create a new VIP resource.
 
-* `name` - (Optional) A unique name for the vip.
+* `ip_address` - (Optional, String, ForceNew) Specifies the IP address desired in the subnet for this VIP.
+  Changing this will create a new VIP resource.
+
+* `name` - (Optional, String) Specifies a unique name for the VIP.
 
 ## Attributes Reference
 
-The following attributes are exported:
+In addition to all arguments above, the following attributes are exported:
 
-* `network_id` - See Argument Reference above.
-* `subnet_id` - See Argument Reference above.
-* `ip_address` - See Argument Reference above.
-* `name` - See Argument Reference above.
-* `status` - The status of vip.
-* `id` - The ID of the vip.
-* `tenant_id` - The tenant ID of the vip.
-* `device_owner` - The device owner of the vip.
+* `id` - The VIP ID.
+
+* `mac_address` - The MAC address of the VIP.
+
+* `status` - The VIP status.
+
+* `device_owner` - The device owner of the VIP.
+
+## Timeouts
+
+This resource provides the following timeouts configuration options:
+
+* `create` - Default is 2 minute.
+* `delete` - Default is 2 minute.
+
+## Import
+
+Network VIPs can be imported using their `id`, e.g.:
+
+```shell
+terraform import flexibleengine_networking_vip_v2.test ce595799-da26-4015-8db5-7733c6db292e
+```
--- flexibleengine/resource_flexibleengine_networking_vip_v2_test.go
+++ flexibleengine/resource_flexibleengine_networking_vip_v2_test.go
@@ -2,17 +2,20 @@ package flexibleengine
 
 import (
 	"fmt"
-	"log"
 	"testing"
 
-	"github.com/chnsz/golangsdk/openstack/networking/v2/ports"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/chnsz/golangsdk/openstack/networking/v1/ports"
 )
 
 // TestAccNetworkingV2VIP_basic is basic acc test.
 func TestAccNetworkingV2VIP_basic(t *testing.T) {
 	var vip ports.Port
+	resourceName := "flexibleengine_networking_vip_v2.vip_1"
+	rName := fmt.Sprintf("tf_test_%s", acctest.RandString(5))
 
 	resource.Test(t, resource.TestCase{
 		PreCheck:     func() { testAccPreCheckFloatingIP(t) },
@@ -20,10 +23,24 @@ func TestAccNetworkingV2VIP_basic(t *testing.T) {
 		CheckDestroy: testAccCheckNetworkingV2VIPDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: TestAccNetworkingV2VIPConfig_basic,
+				Config: testAccNetworkingVIP_basic(rName),
 				Check: resource.ComposeTestCheckFunc(
-					testAccCheckNetworkingV2VIPExists("flexibleengine_networking_vip_v2.vip_1", &vip),
+					testAccCheckNetworkingV2VIPExists(resourceName, &vip),
+					resource.TestCheckResourceAttr(resourceName, "name", rName),
+					resource.TestCheckResourceAttr(resourceName, "ip_version", "4"),
+					resource.TestCheckResourceAttrSet(resourceName, "mac_address"),
 				),
+			},
+			{
+				Config: testAccNetworkingVIP_basic(rName + "_update"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(resourceName, "name", rName+"_update"),
+				),
+			},
+			{
+				ResourceName:      resourceName,
+				ImportState:       true,
+				ImportStateVerify: true,
 			},
 		},
 	})
@@ -32,9 +49,9 @@ func TestAccNetworkingV2VIP_basic(t *testing.T) {
 // testAccCheckNetworkingV2VIPDestroy checks destory.
 func testAccCheckNetworkingV2VIPDestroy(s *terraform.State) error {
 	config := testAccProvider.Meta().(*Config)
-	networkingClient, err := config.NetworkingV2Client(OS_REGION_NAME)
+	vpcClient, err := config.NetworkingV1Client(OS_REGION_NAME)
 	if err != nil {
-		return fmt.Errorf("Error creating FlexibleEngine networking client: %s", err)
+		return fmt.Errorf("Error creating FlexibleEngine VPC v1 client: %s", err)
 	}
 
 	for _, rs := range s.RootModule().Resources {
@@ -42,13 +59,11 @@ func testAccCheckNetworkingV2VIPDestroy(s *terraform.State) error {
 			continue
 		}
 
-		_, err := ports.Get(networkingClient, rs.Primary.ID).Extract()
+		_, err := ports.Get(vpcClient, rs.Primary.ID)
 		if err == nil {
 			return fmt.Errorf("VIP still exists")
 		}
 	}
-
-	log.Printf("[DEBUG] testAccCheckNetworkingV2VIPDestroy success!")
 
 	return nil
 }
@@ -66,12 +81,12 @@ func testAccCheckNetworkingV2VIPExists(n string, vip *ports.Port) resource.TestC
 		}
 
 		config := testAccProvider.Meta().(*Config)
-		networkingClient, err := config.NetworkingV2Client(OS_REGION_NAME)
+		vpcClient, err := config.NetworkingV1Client(OS_REGION_NAME)
 		if err != nil {
-			return fmt.Errorf("Error creating FlexibleEngine networking client: %s", err)
+			return fmt.Errorf("Error creating FlexibleEngine VPC v1 client: %s", err)
 		}
 
-		found, err := ports.Get(networkingClient, rs.Primary.ID).Extract()
+		found, err := ports.Get(vpcClient, rs.Primary.ID)
 		if err != nil {
 			return err
 		}
@@ -79,39 +94,29 @@ func testAccCheckNetworkingV2VIPExists(n string, vip *ports.Port) resource.TestC
 		if found.ID != rs.Primary.ID {
 			return fmt.Errorf("VIP not found")
 		}
-		log.Printf("[DEBUG] test found is: %#v", found)
-		*vip = *found
 
+		*vip = *found
 		return nil
 	}
 }
 
-// TestAccNetworkingV2VIPConfig_basic is used to create.
-var TestAccNetworkingV2VIPConfig_basic = fmt.Sprintf(`
-resource "flexibleengine_networking_network_v2" "network_1" {
-  name = "network_1"
-  admin_state_up = "true"
+func testAccNetworkingVIP_basic(rName string) string {
+	return fmt.Sprintf(`
+resource "flexibleengine_vpc_v1" "vpc_1" {
+  name = "%[1]s"
+  cidr = "192.168.0.0/16"
 }
 
-resource "flexibleengine_networking_subnet_v2" "subnet_1" {
-  name = "subnet_1"
-  cidr = "192.168.199.0/24"
-  ip_version = 4
-  network_id = "${flexibleengine_networking_network_v2.network_1.id}"
-}
-
-resource "flexibleengine_networking_router_interface_v2" "router_interface_1" {
-  router_id = "${flexibleengine_networking_router_v2.router_1.id}"
-  subnet_id = "${flexibleengine_networking_subnet_v2.subnet_1.id}"
-}
-
-resource "flexibleengine_networking_router_v2" "router_1" {
-  name = "router_1"
-  external_gateway = "%s"
+resource "flexibleengine_vpc_subnet_v1" "subnet_1" {
+  name       = "%[1]s"
+  cidr       = "192.168.0.0/24"
+  gateway_ip = "192.168.0.1"
+  vpc_id     = flexibleengine_vpc_v1.vpc_1.id
 }
 
 resource "flexibleengine_networking_vip_v2" "vip_1" {
-  network_id = "${flexibleengine_networking_network_v2.network_1.id}"
-  subnet_id = "${flexibleengine_networking_subnet_v2.subnet_1.id}"
+  name       = "%[1]s"
+  network_id = flexibleengine_vpc_subnet_v1.subnet_1.id
 }
-`, OS_EXTGW_ID)
+`, rName)
+}
