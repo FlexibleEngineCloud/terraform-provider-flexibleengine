4f18ab29a8a17be64b8ea801fd9ff57f2de07d93
Jason-Zhang <jasonzhang9309@163.com>
2023-09-28 15:50:56 +0800

    feat(cce): support hibernate in cce cluster (#1022)

--- flexibleengine/utils.go
+++ flexibleengine/utils.go
@@ -200,3 +200,23 @@ func IsUUIDFormat(str string) bool {
 	}
 	return true
 }
+
+// isStrContainsSliceElement returns true if the string exists in given slice or contains in one of slice elements when
+// open exact flag. Also you can ignore case for this check.
+func isStrContainsSliceElement(str string, sl []string, ignoreCase, isExcat bool) bool {
+	if ignoreCase {
+		str = strings.ToLower(str)
+	}
+	for _, s := range sl {
+		if ignoreCase {
+			s = strings.ToLower(s)
+		}
+		if isExcat && s == str {
+			return true
+		}
+		if !isExcat && strings.Contains(str, s) {
+			return true
+		}
+	}
+	return false
+}
